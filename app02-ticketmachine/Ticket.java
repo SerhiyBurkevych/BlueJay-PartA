ce0695e84032f7cb738157fddb5fb3f1797467eb
SerhiyBurkevych <burkevychsergio@gmail.com>
2020-10-20 19:01:25 +0100

    Did the Ticket

---
+++ app02-ticketmachine/Ticket.java
@@ -0,0 +1,60 @@
+import java.util.Date;
+
+/**
+ * Write a description of class Ticket here.
+ *
+ * @author Serhiy Yaroslavovych Burkevych
+ * @version 20/10/2020
+ */
+public class Ticket
+{
+    private String destination;
+    
+    //this is in pence
+    private int price;
+    
+    private Date datePurchased = new Date();
+   
+
+    /**
+     * Constructor for objects of class Ticket
+     */
+    public Ticket()
+    {
+       price = 0;
+       destination = "Aylesbury";
+    }
+    
+    /**
+     * 
+     */
+    public void setPrice(String destination, int price)
+    {
+       this.price = price;
+       this.destination = destination;
+    }
+    
+    /**
+     * 
+     */public void print()
+    {
+       System.out.println(destination + ", Price"+ price );   
+    }
+       
+    /**
+     * 
+     */
+    public int getPrice()
+    {
+       return price;   
+    }
+    
+    /**
+     * 
+     */
+    public String getDestination()
+    {
+       return destination;   
+    }
+    
+}
