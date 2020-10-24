bae1160be5fbb3e237ee5b241517e7eb2cbd308b
SerhiyBurkevych <burkevychsergio@gmail.com>
2020-10-24 13:23:29 +0100

    App03 done until requirement 10

--- app03-studentGrades/Course.java
+++ app03-studentGrades/Course.java
@@ -8,13 +8,20 @@
  */
 public class Course
 {
-    // Attributes/Variables/Field
-    
     private String title;
     
-    // This is a UCAS code number
+    private String codeNo;
     
-    private String codeNo; 
+    private Module module1;
+    private Module module2;
+    private Module module3;
+    private Module module4;
+    
+    private int noModules;
+    
+    private int finalMark;
+    
+    private Grades finalGrade;
  
     /**
      * Constructor for objects of class Course
@@ -22,12 +29,87 @@ public class Course
     public Course(String title, String code)
     {
         this.title = title;
-        codeNo = code;
+        this.codeNo = code;
+        noModules = 0;
+        finalGrade = Grades.NULL;
+        createModules();
+    }
+    
+    /**
+     * Method that creates modules
+     */
+    public void createModules()
+    {
+        module1 = new Module("Programming Concepts", "CO452");
+        module2 = new Module("3D Modelling", "CO461");
+        module3 = new Module("Computer Architectures", "CO450");
+        module4 = new Module("Game Design", "CO459");
     }
    
+    /**
+     * Print the course title and code number
+     */
     public void print()
     {
        System.out.println("Course: " + title + ", Code: " + codeNo);  
-    }    
+    }
+    
+    public void addModule(Module module)
+    {
+        noModules ++;
+        if(noModules == 1)
+        {
+            module1 = module;
+        }
+    }
+    
+    /**
+     * Method used for setting marks.
+     */
+    public void setModuleMark(int mark, int moduleNo)
+    {
+        if(moduleNo == 1)
+        {
+            module1.setMark(mark);
+        }
+        if(moduleNo == 2)
+        {
+            module2.setMark(mark);
+        }
+        if(moduleNo == 3)
+        {
+            module3.setMark(mark);
+        }
+        if(moduleNo == 4)
+        {
+            module4.setMark(mark);
+        }
+    }
+    
+    public void calculateFinalGrade()
+    {
+        if(finalMark < 40)
+        {
+            finalGrade = Grades.F;
+        }
+        else if((finalMark >= 40) && (finalMark < 50))
+        {
+            finalGrade = Grades.D;
+        }
+         else if((finalMark >= 50) && (finalMark < 59))
+        {
+            finalGrade = Grades.C;
+        }
+         else if((finalMark >= 60) && (finalMark < 69))
+        {
+            finalGrade = Grades.B;
+        }
+         else if((finalMark >= 70) && (finalMark < 100))
+        {
+            finalGrade = Grades.A;
+        }
+        
+    }
+    
  
 }
